a6dd960fab18b8e35bb6859cd04c35e8bbcd0ae5
Props Implementation in React Task 2 <abinayaveeramani@thestackly.com>
2026-06-18 20:49:40 +0530

    component1.jsx

---
+++ component1.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Component2 from "./Component2";
+
+function Component1() {
+  return (
+    <div className="box">
+      <h2>Component 1</h2>
+      <Component2 />
+    </div>
+  );
+}
+
+export default Component1;
